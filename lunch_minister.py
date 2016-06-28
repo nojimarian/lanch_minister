8b4e02db4b76a3bea618589e84596aa9c1f4337d
nojimarian <nojimarian@gmail.com>
2016-06-28 23:57:57 +0900

    readme修正 commonクラス作成

--- lunch_minister.py
+++ lunch_minister.py
@@ -1,20 +1,26 @@
 """ランチ大臣の実行クラス"""
 
+from common import Common
 from slack import Slack
 
-class LunchMinister:
-    """"""
+class LunchMinister(Common):
+    """lunch_time_now!!!"""
 
     def __init__(self):
-        """なにかやることあるかな"""
-        pass
+        """
+        設定ファイルの使う部分をそれぞれセット
+        slackをセット
+        """
+
+        super().__init__()
+        self.team_config = self.configs['Team']
+        self.slack = Slack()
 
     def lunch_time_now(self):
         """実行関数だよ"""
 
-        message = 'みなさん、ランチの時間ですよ'
-        sl_ = Slack()
-        sl_.send(message)
+        message = '{0:s}チームのみなさん、ランチの時間ですよ'.format(self.team_config['Name'])
+        self.slack.send(message)
 
         return 0
 
